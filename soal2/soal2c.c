eac59ee223d654a8695c132819ab918645146f49
ThoriqHuda <81372291+ThoriqHuda@users.noreply.github.com>
2021-05-08 18:51:18 +0700

    Update soal2c.c

--- soal2/soal2c.c
+++ soal2/soal2c.c
@@ -3,30 +3,14 @@
 int pid;
 int pipe1[2];
 int pipe2[2];
-void main() {
-  pipe(pipe1);
-  pid = fork();
-if (pid == 0)
-	exec1();
- pipe(pipe2);
-pid = fork();
- if (pid == 0)
-	exec2();
-  close(pipe1[0]);
-  close(pipe1[1]);
-pid = fork();
-  if (pid == 0)
-	exec3();
-}
 
-void exec3() {
+void three(){
   dup2(pipe2[0], 0);
   close(pipe2[0]);
   close(pipe2[1]);
   execlp("head", "head","-5", NULL);
 }
-void exec2() {
-
+void two(){
   dup2(pipe1[0], 0);
   dup2(pipe2[1], 1);
   close(pipe1[0]);
@@ -35,9 +19,21 @@ void exec2() {
   close(pipe2[1]);
   execlp("sort","sort","-nrk","3,3", NULL);
 }
-void exec1() {
+void one(){
   dup2(pipe1[1], 1);
   close(pipe1[0]);
   close(pipe1[1]);
   execlp("ps", "ps", "aux",NULL);
+}
+int main(){
+  pipe(pipe1);
+  pipe(pipe2);
+ if (fork()==0)
+	one();
+ if (fork()==0)
+	two();
+  close(pipe1[0]);
+  close(pipe1[1]);
+  if (fork()==0)
+	three();
 }
